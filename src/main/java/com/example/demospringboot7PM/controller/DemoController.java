0971b6dc9e876c3f0c5e42f56142738995afa83f
60manojkumar@gmail.com <Motivated@2019>
2022-09-06 19:34:20 +0530

    End Points

--- src/main/java/com/example/demospringboot7PM/controller/DemoController.java
+++ src/main/java/com/example/demospringboot7PM/controller/DemoController.java
@@ -1,7 +1,10 @@
 package com.example.demospringboot7PM.controller;
 
+import com.example.demospringboot7PM.model.Student;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class DemoController {
@@ -23,4 +26,24 @@ public class DemoController {
 
         return welcome;
     }
+
+    @RequestMapping("/course")
+    public ArrayList<String> getCourse(){
+
+        ArrayList<String> course= new ArrayList<>();
+        course.add("Java");
+        course.add("Spring Boot");
+        course.add("Microservices");
+
+        return course;
+    }
+
+    @RequestMapping("/student")
+    public Student getStudent(){
+
+        Student obj= new Student("Raj",23,"CS");
+
+        return obj;
+    }
+
 }
